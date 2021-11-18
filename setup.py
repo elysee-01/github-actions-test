088095d9b10750126a97a6ce5e077acd649a6655
ELYSEE-01 <elyseekevin49@gmail.com>
2021-11-18 12:26:18 +0000

    Commit changes from the auto-formatting action

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from distutils.core import setup
 
 setup(
-  name = 'github_action_test',
-  packages = ['github_action_test'],
-  version = '0.1.0',
-  license='MIT',
-  description = 'GitHub Action workflow test',
-  author = 'KOUA ELYSEE',
-  author_email = 'elyseekevin49@gmail.com',
-  url = 'https://github.com/elysee-01/github_action_test',
-  download_url = 'https://github.com/elysee-01/github_action_test/archive/refs/tags/0.1.0.tar.gz',
-  keywords = ['python', 'github'],
-  package_data={},
-  include_package_data=True,
-  install_requires=[
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-  ],
+    name="github_action_test",
+    packages=["github_action_test"],
+    version="0.1.0",
+    license="MIT",
+    description="GitHub Action workflow test",
+    author="KOUA ELYSEE",
+    author_email="elyseekevin49@gmail.com",
+    url="https://github.com/elysee-01/github_action_test",
+    download_url="https://github.com/elysee-01/github_action_test/archive/refs/tags/0.1.0.tar.gz",
+    keywords=["python", "github"],
+    package_data={},
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
 )
